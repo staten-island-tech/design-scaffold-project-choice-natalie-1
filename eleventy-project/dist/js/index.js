e034721c8ad86c1235db712aa5d956917ff7f3f8
NatalieZ27 <nataliez27@nycstudents.net>
2022-04-28 13:40:59 -0400

    filter

--- eleventy-project/dist/js/index.js
+++ eleventy-project/dist/js/index.js
@@ -8,8 +8,44 @@ document.querySelector(".theme").addEventListener("click", function () {
   }
 });
 
-const blog = document.getElementsByClassName("article-title");
+/* const blog = document.getElementsByClassName("article-title");
 
 const arrayofBlog = Array.from(blog).forEach(function (element) {
   console.log(element);
+}); */
+
+filterObject("all");
+
+function filterObject(c) {
+  var x, i;
+  x = document.getElementsByClassName("article");
+  if (c === "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  }
+}
+
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while ((arr1.indexOf(arr2[i]), -1)) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+document.querySelector(".btn-one").addEventListener("click", function () {
+  filterObject("food");
+});
+
+document.querySelector(".btn-two").addEventListener("click", function () {
+  filterObject("culture");
+});
+
+document.querySelector(".btn-three").addEventListener("click", function () {
+  filterObject("all");
 });
