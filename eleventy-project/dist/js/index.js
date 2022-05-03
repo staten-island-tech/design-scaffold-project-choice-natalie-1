84bd054fa31ab777fef0bd3d47c67879ca450f1d
NatalieZ27 <nataliez27@nycstudents.net>
2022-05-03 13:58:54 -0400

    change

--- eleventy-project/dist/js/index.js
+++ eleventy-project/dist/js/index.js
@@ -14,28 +14,20 @@ const arrayofBlog = Array.from(blog).forEach(function (element) {
   console.log(element);
 }); */
 
-function myFunction() {
-  // Declare variables
-  var input, filter, ul, h3, a, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  ul = document.getElementsByClassName("article");
-  h3 = h3.getElementsByTagName("h3");
+document.getElementById("filter").onkeyup = function () {
+  searchProduct();
+};
 
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = h3[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      h3[i].style.display = "";
-    } else {
-      h3[i].style.display = "none";
-    }
+function searchProduct() {
+  const input = document.getElementById("filter").value.toUpperCase();
+  const blogContainer = document.getElementsByClassName("articles");
+  console.log(blogContainer);
+
+  const blog = blogContainer.getElementsByClassName("article");
+  console.log(blog);
+
+  for (let i = 0; i < blog.length; i++) {
+    let title = blog[i].querySelector(".article-text h3.article-title");
+    console.log(title);
   }
 }
-
-document
-  .querySelector(".input")
-  .addEventListener("input", "onclickup", function () {
-    myFunction();
-  });
